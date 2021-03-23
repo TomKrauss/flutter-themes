739af5a0e06264d073b766319dd8e0d4ca8e9507
Tom Krauss <tom.krauss@gebit.de>
2021-03-23 17:11:46 +0100

    NO-ISSUE: initial version of the Flutter app allowing to dynamically switch themes.

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+void main() {
+  runApp(ThemeDataWidget(child: DemoApplication()));
+}
+
+///
+/// An inherited widget containing a stream of themeData objects to be listened to
+/// in the home widget.
+///
+class ThemeDataWidget extends InheritedWidget {
+  final StreamController<ThemeData> themeDataController =
+      BehaviorSubject.seeded(ThemeData(primarySwatch: Colors.blue));
+
+  ThemeDataWidget({Key? key, required Widget child})
+      : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return themeDataController !=
+        (oldWidget as ThemeDataWidget).themeDataController;
+  }
+
+  static ThemeDataWidget _contextWidget(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<ThemeDataWidget>()!;
+
+  static StreamController<ThemeData> themeStreamController(
+          BuildContext context) =>
+      _contextWidget(context).themeDataController;
+}
+
+class DemoApplication extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: ThemeDataWidget.themeStreamController(context).stream,
+        builder: (context, snapshot) {
+          ThemeData data = snapshot.data != null
+              ? snapshot.data as ThemeData
+              : ThemeData(primarySwatch: Colors.blue);
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: data,
+            home: MyHomePage(title: 'Flutter dynamic Themes Demo'),
+          );
+        });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<ThemeData> _themes = [
+    ThemeData(primarySwatch: Colors.blue),
+    ThemeData(primarySwatch: Colors.amber),
+    ThemeData(primarySwatch: Colors.green),
+    // the following two themes cause bogus message being logged. For details refer to: https://github.com/flutter/flutter/issues/56639
+    ThemeData.dark().copyWith(
+      primaryColor: Colors.grey,
+    ),
+    ThemeData.dark().copyWith(primaryColor: Colors.pink),
+  ];
+  int _currentThemeIdx = 0;
+
+  void _changeTheme() {
+    _currentThemeIdx = (_currentThemeIdx + 1) % _themes.length;
+    ThemeDataWidget.themeStreamController(context)
+        .add(_themes[_currentThemeIdx]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "You've selected theme $_currentThemeIdx:",
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeTheme,
+        tooltip: 'Change Theme',
+        child: Icon(Icons.palette),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
